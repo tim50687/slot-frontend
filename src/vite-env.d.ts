e259b6acca9166c48db522f668179b1ca6126d9c
tim506877 <tim506877@gmail.com>
2025-03-07 00:02:54 -0500

    add environment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,50 @@
 /// <reference types="vite/client" />
+
+import { EIP1193Provider } from "ethers";
+
+// Describe metadata related to a provider based on EIP-6963
+interface EIP6963ProviderInfo {
+  walletId: string;
+  uuid: string;
+  name: string;
+  icon: string;
+}
+
+// Structure of provider based on EIP-1193
+interface EIP1193Provider {
+  isStatus?: boolean;
+  host?: string;
+  path?: string;
+  sendAsync?: (
+    request: { method: string; params?: Array<unknown> },
+    callback: (error: Error | null, response: unknown) => void
+  ) => void;
+  send?: (
+    request: { method: string; params?: Array<unknown> },
+    callback: (error: Error | null, response: unknown) => void
+  ) => void;
+  request: (request: {
+    method: string;
+    params?: Array<unknown>;
+  }) => Promise<unknown>;
+}
+
+// Combines the provider's metadata with an actual provider object
+interface EIP6963ProviderDetail {
+  infor: EIP6963ProviderInfo;
+  provider: EIP1193Provider;
+}
+
+// Custom event that wallet sends to the webpage
+type EIP6963AnnounceProviderEvent = {
+  detail: {
+    info: EIP6963ProviderInfo;
+    provider: EIP1193Provider;
+  };
+};
+
+// EIP-1193 standard error
+interface MMError {
+  code?: string;
+  message?: string;
+}
