cc5b2542396379a4324d5d17948012deda572005
tim506877 <tim506877@gmail.com>
2025-03-07 16:09:20 -0500

    connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { useSyncProviders } from "./hooks/useSyncProviders";
 import "./App.css";
+
 function App() {
   const providers = useSyncProviders();
-  console.log("Providers detected:", providers);
+
   const handleClick = async (providerWithInfo: EIP6963ProviderDetail) => {
     try {
       const account = (await providerWithInfo.provider.request({
