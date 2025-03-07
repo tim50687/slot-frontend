731653150b1cddf2df6589a2a48e642ec0d12958
tim506877 <tim506877@gmail.com>
2025-03-07 00:48:42 -0500

    create providers store

---
+++ src/hooks/store.ts
@@ -0,0 +1,27 @@
+declare global {
+  interface WindowEventMap {
+    "eip6963:announceProvider": CustomEvent;
+  }
+}
+
+let providers: EIP6963ProviderDetail[] = [];
+
+export const store = {
+  value: () => providers,
+  // Takes a callback function to be invoked on each store update
+  subscribe: (callback: () => void) => {
+    function onAnnouncement(event: EIP6963AnnounceProviderEvent) {
+      if (providers.map((p) => p.info.uuid).includes(event.detail.info.uuid)) {
+        return;
+      }
+      providers = [...providers, event.detail];
+      callback();
+    }
+    window.addEventListener("eip6963:announceProvider", onAnnouncement);
+    window.dispatchEvent(new Event("eip6963:announceProvider"));
+
+    // Return a function to unsubscribe from the event
+    return () =>
+      window.removeEventListener("eip6963:announceProvider", onAnnouncement);
+  },
+};
